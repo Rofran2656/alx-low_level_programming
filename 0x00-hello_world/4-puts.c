d8a21d80dce97a8a840447ad11bf125b7db7c0fe
Rofran2656 <mutanufrancy@gmail.com>
2023-06-14 23:23:06 -0700

    task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Programming is like building a multilingual puzzle
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
