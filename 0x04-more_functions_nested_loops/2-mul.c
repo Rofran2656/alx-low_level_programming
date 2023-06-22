c32aaa6c32ed48e9eab4bce78d05f3a3a92d7a31
Rofran2656 <mutanufrancy@gmail.com>
2023-06-22 07:24:21 -0700

    correction task 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - a function that multiplies two integers
+ * mul - a function that multiplies two integers
  * @x: first number to multiply
  * @y: second number to multiply the first number
  * Return: result of multiplication of x and y
