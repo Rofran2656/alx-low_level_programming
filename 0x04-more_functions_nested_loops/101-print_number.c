23320ca43579b6d7f87ae520f5f938e7980d919b
Rofran2656 <mutanufrancy@gmail.com>
2023-06-23 09:29:14 -0700

    task 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - printing an integer
+ * @n: parameter for input integer
+ * Return: empty
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
