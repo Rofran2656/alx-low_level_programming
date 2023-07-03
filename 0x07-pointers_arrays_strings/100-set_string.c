b3ace85999f48db91e15652479ff7de1e7b5d891
Rofran2656 <mutanufrancy@gmail.com>
2023-07-03 16:49:35 -0700

    task 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - entry point
+ * @s: pointer to pointer
+ * @to: pointer char
+ * Return: always 0 (success)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
